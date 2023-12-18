1cde78c30c1b06392df99ad892d1d73f7780288f
MotiurRahmanSany <mdmotiurrahmansany@gmail.com>
2023-12-18 13:46:35 +0600

    added

---
+++ Codeforces/A_Way_Too_Long_Words.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+void solveIt(){
+    string s;
+    cin >> s;
+    int n = s.size();
+    if(n > 4)
+        cout << s[0] << n - 2 << s[n - 1] << '\n';
+    else cout << s << '\n';
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+	int tt;
+    cin >> tt;
+    while(tt--){
+        solveIt();            
+    }
+    
+	return 0;
+}
